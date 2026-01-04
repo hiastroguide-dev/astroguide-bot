f1e56e3de56339236e66072a29f7e11146da2341
hiastroguide-dev <hiastroguide@gmail.com>
2026-01-05 00:36:47 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,32 @@
+import requests
+from flask import Flask, request
+
+BOT_TOKEN = "8504453160: AAH3XyoU970-
+TnME9X1eHuLEW7h5j9iuro"
+TELEGRAM_API = f"https://api.telegram.org/bot{BOT_TOKEN}"
+
+app = Flask(__name__)
+
+@app.route("/", methods=["POST"])
+def webhook():
+    data = request.json
+    message = data.get("message", {})
+    chat = message.get("chat", {})
+    chat_id = chat.get("id")
+    text = message.get("text", "")
+
+    if text == "/start":
+        send_message(chat_id, "Hello 👋 I am AstroGuide.")
+
+    return "ok"
+
+def send_message(chat_id, text):
+    url = f"{TELEGRAM_API}/sendMessage"
+    payload = {
+        "chat_id": chat_id,
+        "text": text
+    }
+    requests.post(url, json=payload)
+
+if __name__ == "__main__":
+    app.run()
